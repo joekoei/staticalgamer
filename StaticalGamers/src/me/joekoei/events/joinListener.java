dc758736e6194a68743ad75f722d4cb1db4485c8
BuildTools <unconfigured@null.spigotmc.org>
2018-12-02 18:02:33 +0100

    Event updated

--- StaticalGamers/src/me/joekoei/events/joinListener.java
+++ StaticalGamers/src/me/joekoei/events/joinListener.java
@@ -1,16 +1,30 @@
 package me.joekoei.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class joinListener implements Listener {
 
 	public void onJoin(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
 		
-		if(p.isOp() ) {
-			
+		if(p.isOp()) {
+			Bukkit.broadcastMessage(p + " Joined the server! (Crew)");
+		} else {
+			Bukkit.broadcastMessage(p + " Joined the server!");
+		}
+	}
+	
+	public void onQuit(PlayerQuitEvent e) {
+		Player p = e.getPlayer();
+		
+		if(p.isOp()) {
+			Bukkit.broadcastMessage(p + " Just disconnected (Crew");
+		} else {
+			Bukkit.broadcastMessage(p + " Just disconnected");
 		}
 	}
 	
