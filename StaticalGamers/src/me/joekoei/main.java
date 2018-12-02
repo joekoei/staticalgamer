ff4505480550e94742b40dcd4ef3d0b435482cf8
BuildTools <unconfigured@null.spigotmc.org>
2018-12-02 17:34:05 +0100

    Project

---
+++ StaticalGamers/src/me/joekoei/main.java
@@ -0,0 +1,24 @@
+package me.joekoei;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+
+	public void onEnable() {
+		events();
+		commands();
+		System.out.println("Loaded StaticalGamer Core");
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public void events() {
+		
+	}
+	
+	public void commands() {
+		
+	}
+}
