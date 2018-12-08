e79253e487ff4d673e958a8fd5a697e4b202b114
BuildTools <unconfigured@null.spigotmc.org>
2018-12-08 21:55:50 +0100

    Nieuw Listener / Event ja

---
+++ StaticalGamers/src/me/joekoei/events/breakListener.java
@@ -0,0 +1,16 @@
+package me.joekoei.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class breakListener implements Listener{
+
+	public void OnBreak(BlockBreakEvent e) {
+		Player p = e.getPlayer();
+		
+		if(!p.isOp());
+			e.setCancelled(true);
+	}
+	
+}
