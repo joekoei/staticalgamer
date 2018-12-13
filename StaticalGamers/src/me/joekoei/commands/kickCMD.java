56241feb3fc03944b230c86c83949fecf312305a
BuildTools <unconfigured@null.spigotmc.org>
2018-12-13 16:28:27 +0100

    Updated

--- StaticalGamers/src/me/joekoei/commands/kickCMD.java
+++ StaticalGamers/src/me/joekoei/commands/kickCMD.java
@@ -9,8 +9,11 @@ public class kickCMD implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("kick")); {
-			if(!sender.isOp());
+			if(!sender.isOp()) {
 				sender.sendMessage("Je bent geen staff vies kind!");
+			} else {
+				sender.sendMessage("Nog niet af!");
+			}
 		}
 		return false;
 	}
